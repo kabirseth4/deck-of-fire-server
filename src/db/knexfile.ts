8cb0139266ceb123a24bb90487c3c6249a1c5f84
kabirseth4 <kabirseth4@gmail.com>
2024-05-09 17:22:19 +0100

    Refactor: remove repeated configs from knexfile

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -16,54 +16,36 @@ const {
   TEST_DB_NAME: testDatabase,
 } = process.env;
 
+const defaults = {
+  client: "mysql2",
+  connection: {
+    host,
+    database,
+    user,
+    password,
+    charset: "utf8",
+  },
+  migrations: {
+    directory: join(__dirname, "/migrations"),
+  },
+  seeds: {
+    directory: join(__dirname, "/seeds"),
+  },
+};
+
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "mysql2",
-    connection: {
-      host,
-      database,
-      user,
-      password,
-      charset: "utf8",
-    },
-    migrations: {
-      directory: join(__dirname, "/migrations"),
-    },
-    seeds: {
-      directory: join(__dirname, "/seeds"),
-    },
+    ...defaults,
   },
   test: {
-    client: "mysql2",
+    ...defaults,
     connection: {
-      host,
+      ...defaults.connection,
       database: testDatabase,
-      user,
-      password,
-      charset: "utf8",
-    },
-    migrations: {
-      directory: join(__dirname, "/migrations"),
-    },
-    seeds: {
-      directory: join(__dirname, "/seeds"),
     },
   },
   production: {
-    client: "mysql2",
-    connection: {
-      host,
-      database,
-      user,
-      password,
-      charset: "utf8",
-    },
-    migrations: {
-      directory: join(__dirname, "/migrations"),
-    },
-    seeds: {
-      directory: join(__dirname, "/seeds"),
-    },
+    ...defaults,
   },
 };
 
